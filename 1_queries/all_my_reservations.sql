5d787492c74398d975b7cc358442a6807cf9dbbb
CamilaRivera <camila.riverart@gmail.com>
2019-09-19 19:04:38 +0000

    User functions connected to the database

--- 1_queries/all_my_reservations.sql
+++ 1_queries/all_my_reservations.sql
@@ -6,3 +6,29 @@ WHERE reservations.guest_id = 1 AND now()::date > reservations.end_date
 GROUP BY properties.id,  reservations.start_date
 ORDER BY start_date ASC;
 
+SELECT properties.id, properties.title, properties.cost_per_night, reservations.start_date, AVG (rating)
+FROM reservations 
+JOIN properties ON properties.id = reservations.property_id
+JOIN property_reviews ON property_reviews.reservation_id = reservations.id
+WHERE reservations.guest_id = 1 AND now()::date > reservations.end_date
+GROUP BY properties.id,  reservations.start_date
+ORDER BY start_date ASC;
+
+
+
+SELECT count(*)
+FROM reservations 
+JOIN properties ON properties.id = reservations.property_id
+JOIN property_reviews ON property_reviews.property_id = properties.id
+WHERE reservations.guest_id = 1 AND now()::date > reservations.end_date
+GROUP BY properties.id,  reservations.start_date
+ORDER BY start_date ASC;
+
+SELECT count(*)
+FROM reservations 
+JOIN properties ON properties.id = reservations.property_id
+JOIN property_reviews ON property_reviews.reservation_id = reservations.id
+WHERE reservations.guest_id = 1 AND now()::date > reservations.end_date
+GROUP BY properties.id,  reservations.start_date
+ORDER BY start_date ASC;
+
